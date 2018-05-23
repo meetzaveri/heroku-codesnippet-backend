e789e31a7c61cca0eb1e1ca26379cc8c15b7a9e5
Meet Zaveri <meet.zaveri@Simforms-161.local>
2018-05-23 12:28:23 +0530

    Seperated controllers into user and auth.

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const contentRoutes = require('./authcontroller.js');
+const userRoutes = require('./usercontroller.js');
 
 module.exports = function(app, db) {
   contentRoutes(app, db);
+  userRoutes(app,db);
   // Other route groups could go here, in the future
 };
